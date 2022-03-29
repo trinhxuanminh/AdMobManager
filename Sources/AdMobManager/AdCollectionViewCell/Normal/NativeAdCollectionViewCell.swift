548d80cee96707210283dc51f597abba2cb15300
Trịnh Xuân Minh <trinhxuanminh2000@gmail.com>
2022-03-29 22:11:50 +0700

    update multiple NativeAd in cell

--- Sources/AdMobManager/AdCollectionViewCell/Normal/NativeAdCollectionViewCell.swift
+++ Sources/AdMobManager/AdCollectionViewCell/Normal/NativeAdCollectionViewCell.swift
@@ -171,13 +171,12 @@ public class NativeAdCollectionViewCell: UICollectionViewCell {
             }
             self.listNativeAd.append(NativeAd())
         }
-        if listNativeAd[index] == nil {
-            listNativeAd[index] = NativeAd()
+        if self.listNativeAd[index] == nil {
+            self.listNativeAd[index] = NativeAd()
         }
-        let nativeAd = listNativeAd[index]
-        self.config_Data(ad: nativeAd?.get_Ad())
-        nativeAd?.set_Config_Data { [weak self] in
-            self?.config_Data(ad: nativeAd?.get_Ad())
+        self.config_Data(ad: self.listNativeAd[index]?.get_Ad())
+        self.listNativeAd[index]?.set_Config_Data { [weak self] in
+            self?.config_Data(ad: self?.listNativeAd[index]?.get_Ad())
         }
     }
 }
