e15932e5d1e6ce644f59c11d5f2acf2fb56c5c36
Trịnh Xuân Minh <trinhxuanminh2000@gmail.com>
2022-03-29 11:38:44 +0700

    test deinit NativeAd

--- Sources/AdMobManager/AdView/Normal/NativeAdView.swift
+++ Sources/AdMobManager/AdView/Normal/NativeAdView.swift
@@ -64,7 +64,7 @@ import NVActivityIndicatorView
     }
     
     fileprivate var didConfigData: Bool = false
-    fileprivate var nativeAd: NativeAd? = NativeAd()
+    fileprivate var nativeAd: NativeAd?
     
     public override func awakeFromNib() {
         super.awakeFromNib()
@@ -91,6 +91,10 @@ import NVActivityIndicatorView
         self.nativeAd?.cancelRequest()
         self.nativeAd = nil
         super.removeFromSuperview()
+    }
+    
+    deinit {
+        print("NativeAdView deinit")
     }
     
     /// This function helps to adjust the color of the ad content.
@@ -182,6 +186,7 @@ extension NativeAdView {
     }
     
     func setAd() {
+        self.nativeAd = NativeAd()
         self.config_Data(ad: self.nativeAd?.get_Ad())
         self.nativeAd?.set_Config_Data { [weak self] in
             self?.config_Data(ad: self?.nativeAd?.get_Ad())
