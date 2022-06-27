19a4877f834d9fa52a2747182fbfd5c3cf74efac
Trịnh Xuân Minh <trinhxuanminh2000@gmail.com>
2022-06-27 09:25:48 +0700

    debug

--- Sources/AdMobManager/AdView/Normal/NativeAdView.swift
+++ Sources/AdMobManager/AdView/Normal/NativeAdView.swift
@@ -138,6 +138,7 @@ import SkeletonView
 extension NativeAdView {
   func config_Data(ad: GADNativeAd?) {
     guard let nativeAd = ad else {
+      skeletonView.layoutIfNeeded()
       skeletonView.showAnimatedGradientSkeleton(
         usingGradient: SkeletonGradient(
           baseColor: baseColor,
