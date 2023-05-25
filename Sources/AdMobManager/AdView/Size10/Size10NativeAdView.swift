e1beb7ec484de5dd1e6ac29016a4fc6b46815a29
Minh Trịnh Xuân <trinhxuanminh2000@gmail.com>
2023-05-25 14:21:45 +0700

    update size 10

--- Sources/AdMobManager/AdView/Size10/Size10NativeAdView.swift
+++ Sources/AdMobManager/AdView/Size10/Size10NativeAdView.swift
@@ -67,7 +67,8 @@ import NVActivityIndicatorView
   }
   
   override func setProperties() {
-    clipsToBounds = true
+    nativeAdView.layer.cornerRadius = 36.5
+    nativeAdView.layer.borderWidth = 0.5
     
     iconImageView.clipsToBounds = true
     iconImageView.layer.cornerRadius = 10.0
@@ -78,17 +79,19 @@ import NVActivityIndicatorView
   }
   
   override func setColor() {
-    changeLoading(color: UIColor(rgb: 0x000000))
+    nativeAdView.layer.borderColor = UIColor.white.cgColor
     
-    iconImageView.backgroundColor = UIColor(rgb: 0xD9D9D9)
+    changeLoading(color: UIColor.white)
+    
+    iconImageView.backgroundColor = UIColor.gray
     
     adLabel.backgroundColor = .clear
-    adLabel.textColor = UIColor(rgb: 0x39BA19)
-    adLabel.layer.borderColor = UIColor(rgb: 0x39BA19).cgColor
+    adLabel.textColor = UIColor.white
+    adLabel.layer.borderColor = UIColor.white.cgColor
     
-    headlineLabel.textColor = UIColor(rgb: 0x000000)
+    headlineLabel.textColor = UIColor.white
     
-    bodyLabel.textColor = UIColor(rgb: 0x27303E)
+    bodyLabel.textColor = UIColor.white
   }
   
   /// This function returns the minimum recommended height.
@@ -113,11 +116,15 @@ import NVActivityIndicatorView
   }
   
   public func changeColor(
+    border: UIColor? = nil,
     title: UIColor? = nil,
     ad: UIColor? = nil,
     adBackground: UIColor? = nil,
     body: UIColor? = nil
   ) {
+    if let border = border {
+      nativeAdView.layer.borderColor = border.cgColor
+    }
     if let title = title {
       headlineLabel.textColor = title
     }
