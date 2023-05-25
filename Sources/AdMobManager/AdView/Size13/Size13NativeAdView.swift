98186eccf55ae0379623b12f501fd5feca37c459
Minh Trịnh Xuân <trinhxuanminh2000@gmail.com>
2023-05-25 14:32:13 +0700

    update size 13

--- Sources/AdMobManager/AdView/Size13/Size13NativeAdView.swift
+++ Sources/AdMobManager/AdView/Size13/Size13NativeAdView.swift
@@ -67,7 +67,8 @@ import NVActivityIndicatorView
   }
   
   override func setProperties() {
-    changeLoading(color: UIColor(rgb: 0x000000))
+    nativeAdView.layer.cornerRadius = 8.0
+    nativeAdView.layer.borderWidth = 0.5
     
     mediaView.clipsToBounds = true
     mediaView.layer.cornerRadius = 8.0
@@ -81,16 +82,20 @@ import NVActivityIndicatorView
   }
   
   override func setColor() {
+    nativeAdView.layer.borderColor = UIColor.white.cgColor
+    
+    changeLoading(color: UIColor.white)
+    
     adLabel.backgroundColor = .clear
-    adLabel.textColor = UIColor(rgb: 0x39BA19)
-    adLabel.layer.borderColor = UIColor(rgb: 0x39BA19).cgColor
+    adLabel.textColor = UIColor.white
+    adLabel.layer.borderColor = UIColor.white.cgColor
     
-    headlineLabel.textColor = UIColor(rgb: 0x000000)
+    headlineLabel.textColor = UIColor.white
     
-    bodyLabel.textColor = UIColor(rgb: 0x27303E)
+    bodyLabel.textColor = UIColor.white
     
-    callToActionButton.setTitleColor(UIColor(rgb: 0xFFFFFF), for: .normal)
-    callToActionButton.backgroundColor = UIColor(rgb: 0x00A9A8)
+    callToActionButton.setTitleColor(UIColor.black, for: .normal)
+    callToActionButton.backgroundColor = UIColor(rgb: 0x90EBFF)
   }
   
   /// This function returns the minimum recommended height.
@@ -116,6 +121,7 @@ import NVActivityIndicatorView
   }
   
   public func changeColor(
+    border: UIColor? = nil,
     title: UIColor? = nil,
     ad: UIColor? = nil,
     adBackground: UIColor? = nil,
@@ -124,6 +130,9 @@ import NVActivityIndicatorView
     callToAction: UIColor? = nil,
     callToActionBackground: UIColor? = nil
   ) {
+    if let border = border {
+      nativeAdView.layer.borderColor = border.cgColor
+    }
     if let title = title {
       headlineLabel.textColor = title
     }
