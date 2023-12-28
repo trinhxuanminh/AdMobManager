05904182e9cd8dece75b47ab74a7ad394c8fe77a
trinhxuanminh2000 <trinhxuanminh2000@gmail.com>
2023-12-28 13:51:15 +0700

    Update frequency

--- Sources/AdMobManager/FrequencyManager.swift
+++ Sources/AdMobManager/FrequencyManager.swift
@@ -10,7 +10,15 @@ import Foundation
 class FrequencyManager {
   static let shared = FrequencyManager()
   
+  enum Keys {
+    static let cache = "FrequencyCache"
+  }
+  
   private var countClicks: [String: Int] = [:]
+  
+  init() {
+    fetch()
+  }
 }
 
 extension FrequencyManager {
@@ -21,5 +29,19 @@ extension FrequencyManager {
   func increaseCount(name: String) {
     let count = getCount(name: name)
     countClicks[name] = count + 1
+    save()
+  }
+}
+
+extension FrequencyManager {
+  private func fetch() {
+    guard let countClicks = UserDefaults.standard.dictionary(forKey: Keys.cache) as? [String: Int] else {
+      return
+    }
+    self.countClicks = countClicks
+  }
+  
+  private func save() {
+    UserDefaults.standard.set(countClicks, forKey: Keys.cache)
   }
 }
